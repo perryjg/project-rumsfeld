dc71174b405eaab523719b3587666bb511422fb5
Daniel Lathrop <dlathrop@dallasnews.com>
2012-04-24 13:32:42 -0500

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rumsfeld::Application.config.secret_token = '7f3453945b65d7550d12e2c29456638501ddbb2ac5e30eee14122a2f2992f42cc65b013ad80f897b7ab11d02087eed8825070366a41fbeb3193d88c074bc0edd'
