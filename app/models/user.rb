1b9b3c09d6d170f196e5f89c4b2017601f267769
John Perry <jgperry@ajc.com>
2012-05-03 15:38:28 -0400

    user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id           :integer         not null, primary key
+#  name         :string(255)
+#  email        :string(255)
+#  phone        :string(255)
+#  title        :string(255)
+#  organization :string(255)
+#  address      :string(255)
+#  city         :string(255)
+#  state        :string(255)
+#  zip          :string(255)
+#  created_at   :datetime        not null
+#  updated_at   :datetime        not null
+#
+
 class User < ActiveRecord::Base
+	attr_accessible :name, :email, :phone, :title, :organization, :address,
+	                :city, :state, :zip
+
+	validates :name, presence: true
+	validates :email, presence: true
 end
