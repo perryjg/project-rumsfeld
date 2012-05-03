8caeaac57107967b5da9c3717179e4f799d7720c
John Perry <jgperry@ajc.com>
2012-05-03 14:28:11 -0400

    style pages with bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,32 @@
 require 'spec_helper'
 
 describe 'Static Pages' do
-	before { visit '/static_pages/home'}
+	subject { page }
+	before { visit root_path}
 
 	describe	'Home page' do
-		it 'should have content "Rumsfeld"' do
-			page.should have_selector('h1', :text => 'Rumsfeld')
-		end
-
-		it 'should have the right title' do
-			page.should have_selector( 'title', :text => 'Rumsfeld | Home' )
-		end
+		it { should have_selector('h1', :text => 'Rumsfeld') }
+		it { should have_selector( 'title', :text => 'Rumsfeld | Home' ) }
 	end
 
 	describe	'Help page' do
-		before { visit '/static_pages/help' }
+		before { visit help_path }
 
-		it 'should have content "Help"' do
-			page.should have_selector('h1', :text => 'Help')
-		end
-
-		it 'should have the right title' do
-			page.should have_selector( 'title', :text => 'Rumsfeld | Help' )
-		end
+		it { should have_selector('h1', :text => 'Help') }
+		it { should have_selector( 'title', :text => 'Rumsfeld | Help' ) }
 	end
 
 	describe	'About page' do
-		before { visit '/static_pages/about' }
+		before { visit about_path }
 
-		it 'should have content "About"' do
-			page.should have_selector('h1', :text => 'About')
-		end
+		it { should have_selector('h1', :text => 'About') }
+		it { should have_selector( 'title', :text => 'Rumsfeld | About' ) }
+	end
 
-		it 'should have the right title' do
-			page.should have_selector( 'title', :text => 'Rumsfeld | About' )
-		end
+	describe	'Contact page' do
+		before { visit contact_path }
+
+		it { should have_selector('h1', :text => 'Contact') }
+		it { should have_selector( 'title', :text => 'Rumsfeld | Contact' ) }
 	end
 end
