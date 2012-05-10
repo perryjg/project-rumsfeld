37b131a2136e022995f9f3475df8acfbd0644eea
John Perry <jgperry@ajc.com>
2012-05-10 18:07:22 -0400

    request list on user page; new request form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @requests = @user.requests
 
     respond_to do |format|
       format.html # show.html.erb
---
+++ spec/requests/request_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "RequestPages" do
+  describe "GET /request_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      "pending"
+    end
+  end
+end
