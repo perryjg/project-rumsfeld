500330d4cf7e237ec2363904d3a33c259aeac2ab
John Perry <jgperry@ajc.com>
2012-06-06 15:27:29 -0400

    style tweeks

--- app/controllers/print_letters_controller.rb
+++ app/controllers/print_letters_controller.rb
@@ -5,7 +5,10 @@ class PrintLettersController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @request_types }
-      format.pdf  { render pdf: "letter_#{@request.id}" }
+      format.pdf  {
+      	render pdf: "letter_#{@request.id}",
+      	       margin: { top: 20, bottom: 20, left: 25, right: 25 }
+      }
     end
   end
 end
